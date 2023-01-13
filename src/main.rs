2e3d952f9063ac1a5ac115540e2e40e1fdf7c7de
jsgthb <jeff.schaack@gmail.com>
2023-01-13 20:33:02 +0100

    Hash password on entry

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ mod aes_cryptography;
 fn main() {
     let file_path = interface::get_file_path();
     let method = interface::select_method();
-    let plaintext_password = interface::enter_password();
+    let password = aes_cryptography::hash_password(interface::enter_password());
     // Encrypt or decrypt
     match method.as_str() {
-        "Encrypt" => println!("Encrypt file"),
-        _=> println!("Decrypt file")
+        "Encrypt" => println!("{}; {}; {}", file_path, method, password),
+        _=> println!("{}; {}; {}", file_path, method, password)
     }
 }
